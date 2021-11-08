ffadc20bb64cbd511deecd19c0f76fe657b61830
apalencia45 <89164512+apalencia45@users.noreply.github.com>
2021-11-08 17:43:39 -0600

    energia

--- proyecto_2/proyecto_2.ino
+++ proyecto_2/proyecto_2.ino
@@ -1,9 +1,50 @@
+/*******************************
+//Universidad del Valle de Guatemala
+//BE3015: Electrónica Digital 2
+//Andrea Palencia
+//Proyecto 2
+
+//*******************************
+
+//Definición de pines 
+#define tx PD_7
+#define rx PD_6
+#define boton1 PD_4
+#define boton2 PD_0
+
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial2.begin(115200);
+  pinMode(boton1, INPUT_PULLUP);
+  pinMode(boton2, INPUT_PULLUP);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+ 
+  int EstadoBI = digitalRead(BtnI);
+  int EstadoBD = digitalRead(BtnD);
+
+  if(EstadoBI == LOW)
+  {
+    DCLedG++;
+    delay(50);
+    if (DCLedG > 254)
+    {
+      DCLedG == 0;
+    }
+  }
   
+  if(EstadoBD == LOW)
+  {
+    DCLedG--;
+    delay(50);
+    if (DCLedG < 1)
+    {
+      DCLedG == 253;
+    }
+  }
 }
